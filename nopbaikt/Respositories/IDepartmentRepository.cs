aeaff1867415281d5aef5f7857c1f434305270b4
Tienanhmr <118037885+Tienanhmr@users.noreply.github.com>
2023-12-06 23:19:29 +0700

    Delete nopbaikt/Respositories directory

--- nopbaikt/Respositories/IDepartmentRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿
-public interface IDepartmentRepository
-{
-    IEnumerable<Department> GetDepartments();
-    Department GetDepartmentById(int id);
-    void AddDepartment(Department department);
-    void UpdateDepartment(Department department);
-    void DeleteDepartment(int id);
-}
--- nopbaikt/Respositories/IEmployeeRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿// Repositories/IEmployeeRepository.cs
-public interface IEmployeeRepository
-{
-    IEnumerable<Employee> GetEmployeesByDepartment(int departmentId);
-    void AddEmployee(Employee employee);
-    // Other CRUD methods for employees
-}
