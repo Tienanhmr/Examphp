48847867a9b351a41de27e161a4e557f0fdf9241
Tienanhmr <118037885+Tienanhmr@users.noreply.github.com>
2023-12-06 23:19:14 +0700

    Delete nopbaikt/Models directory

--- nopbaikt/Models/Department.cs
+++
@@ -1,9 +0,0 @@
-﻿// Models/Department.cs
-public class Department
-{
-    public int Id { get; set; }
-    public string DepartmentName { get; set; }
-    public string DepartmentCode { get; set; }
-    public string Location { get; set; }
-    public int NumberOfPersonals { get; set; }
-}
